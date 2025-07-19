759ea2ade81c718e62fedc8de03bf65a5b5a7b4d
Isaac Watts <Isaac.s.watts@gmail.com>
2025-07-18 21:12:53 -0400

    Added telescope to my neovim configuration

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,8 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+local builtin = require('telescope.builtin')
+map('n', 'ff', builtin.find_files, { desc = 'Telescope find files' })
+map('n', 'fg', builtin.live_grep, { desc = 'Telescope live grep' })
+map('n', 'fb', builtin.buffers, { desc = 'Telescope buffers' })
+map('n', 'fh', builtin.help_tags, { desc = 'Telescope help tags' })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,6 +13,10 @@ return {
     end,
   },
 
+    {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
